038736833597bfb804b093bea482b85941845202
SeongMin-K <eo1tks3@naver.com>
2021-09-18 17:00:13 +0900

    MoveViewUpBtn

--- Preview/ViewController.swift
+++ Preview/ViewController.swift
@@ -8,54 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // 클로저로 뷰를 설정
-    var mySecondView: UIView = {
-        let view = UIView()
-        view.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-        view.layer.cornerRadius = 16
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.clipsToBounds = true
-        return view
-    }()
     
-    var myThirdView: MyCircleView = {
-        let circleView = MyCircleView()
-        circleView.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
-        circleView.translatesAutoresizingMaskIntoConstraints = false
-        return circleView
-    }()
+    var someViewBottomConstraint: NSLayoutConstraint?
+
+    override func loadView() {
+        super.loadView()
+        print("ViewController - loadView() called")
+        
+        view.backgroundColor = UIColor(named: "MyYellow")
+        
+        let someView = UIView()
+        someView.backgroundColor = UIColor(named: "MyBlue")
+        someView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(someView)
+        someView.layer.cornerRadius = 8
+        NSLayoutConstraint.activate([
+            someView.widthAnchor.constraint(equalToConstant: 100),
+            someView.heightAnchor.constraint(equalToConstant: 50),
+            someView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            //someView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        someViewBottomConstraint = NSLayoutConstraint(item: someView, attribute: .bottom, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 0)
+        someViewBottomConstraint?.isActive = true
+
+        
+        let moveViewUpBtn = UIButton(type: .system)
+        moveViewUpBtn.translatesAutoresizingMaskIntoConstraints = false
+        moveViewUpBtn.backgroundColor = .white
+        moveViewUpBtn.setTitle("위로 올리기", for: .normal)
+        moveViewUpBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        moveViewUpBtn.setTitleColor(.black, for: .normal)
+        moveViewUpBtn.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        moveViewUpBtn.layer.cornerRadius = 8
+        moveViewUpBtn.addTarget(self, action: #selector(moveViewUp), for: .touchUpInside)
+        self.view.addSubview(moveViewUpBtn)
+        moveViewUpBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        moveViewUpBtn.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 150).isActive = true
+    }
+    
+    @objc fileprivate func moveViewUp() {
+        print("ViewController - moveViewUp() called")
+        
+        someViewBottomConstraint?.constant -= 50
+        
+        UIViewPropertyAnimator(duration: 0.3, curve: .easeOut, animations: { [weak self] in
+            guard let self = self else { return }
+            self.view.layoutIfNeeded()
+        }).startAnimation()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let myFirstView = UIView()
-        myFirstView.translatesAutoresizingMaskIntoConstraints = false
-        myFirstView.backgroundColor = .systemRed
-        self.view.addSubview(myFirstView)
-        
-        // FirstView
-        myFirstView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        myFirstView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150).isActive = true
-        myFirstView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        myFirstView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        myFirstView.layer.cornerRadius = 30
-        
-        // SecondView
-        self.view.addSubview(mySecondView)
-        NSLayoutConstraint.activate([
-            mySecondView.leadingAnchor.constraint(equalTo: myFirstView.leadingAnchor, constant: 10),
-            mySecondView.topAnchor.constraint(equalTo: myFirstView.bottomAnchor, constant: 50),
-            mySecondView.widthAnchor.constraint(equalToConstant: 100),
-            mySecondView.heightAnchor.constraint(equalToConstant: 100)
-        ])
-        
-        self.view.addSubview(myThirdView)
-        myThirdView.widthAnchor.constraint(equalTo: mySecondView.widthAnchor, multiplier: 1.5).isActive = true
-        myThirdView.heightAnchor.constraint(equalTo: mySecondView.heightAnchor, multiplier: 1.5).isActive = true
-        myThirdView.topAnchor.constraint(equalTo: mySecondView.bottomAnchor, constant: 50).isActive = true
-        myThirdView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
     }
 }
 
@@ -77,6 +83,7 @@ struct ViewControllerRepresentable: UIViewControllerRepresentable {
 struct ViewController_Previews: PreviewProvider {
     static var previews: some View {
         ViewControllerRepresentable()
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         .previewDisplayName("iPhone 12 Pro")
     }
 }
